5eba531e89d143aa775e1c37f171dcf6ce130a14
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2026-02-28 18:02:17 +0300

    -

--- waybar/.config/waybar/scripts/theme-status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-current=$(gsettings get org.gnome.desktop.interface color-scheme)
-if [[ $current == "'prefer-dark'" ]]; then
-  echo '{"text": "🌙", "tooltip": "Dark Mode"}'
-else
-  echo '{"text": "☀️", "tooltip": "Light Mode"}'
-fi
--- waybar/.config/waybar/scripts/toggle-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current=$(gsettings get org.gnome.desktop.interface color-scheme)
-if [[ $current == "'prefer-dark'" ]]; then
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-else
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-fi
-killall -SIGUSR2 waybar
